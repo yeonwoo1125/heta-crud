d863eca8be7e2b260390719c6172dca9f6516ce8
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-08-17 22:20:18 +0900

    가독성을 위한 라인 정리

--- src/backend/user/model/UserModel.js
+++ src/backend/user/model/UserModel.js
@@ -5,13 +5,22 @@ export default class UserModel extends HetaSequelize {
     init(Sequelize, options) {
 
         return this.model('user', {
-                id: {type: Sequelize.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true},
-                name: {type: Sequelize.STRING(200), allowNull: false},
-                age: {type: Sequelize.INTEGER.UNSIGNED, allowNull: false}
+                id: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                name: {
+                    type: Sequelize.STRING(200),
+                    allowNull: false
+                },
+                age: {
+                    type: Sequelize.INTEGER.UNSIGNED,
+                    allowNull: false
+                }
             },
             options
         );
-
     }
 
     static associate(models) {
